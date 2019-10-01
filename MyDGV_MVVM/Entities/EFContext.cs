a274d5ae7692a532f570f0d45766b331ef1917f9
Stepan <stepansmetanskyy@gmail.com>
2019-10-01 20:41:34 +0300

    Added entities (class person & class EFContext)

---
+++ MyDGV_MVVM/Entities/EFContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDGV_MVVM.Entities
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("") { }
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ MyDGV_MVVM/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDGV_MVVM.Entities
+{
+    [Table ("tblPeople")]
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+        public bool Gender { get; set; }
+    }
+}
