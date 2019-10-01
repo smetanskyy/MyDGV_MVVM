abf75e9e2470350ef749ec273d620f65384c2233
Stepan <stepansmetanskyy@gmail.com>
2019-10-01 22:03:41 +0300

    Added random people to database

--- MyDGV_MVVM/MainWindow.xaml.cs
+++ MyDGV_MVVM/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyDGV_MVVM.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static MyDGV_MVVM.Helpers.Helper;
 
 namespace MyDGV_MVVM
 {
@@ -20,9 +22,46 @@ namespace MyDGV_MVVM
     /// </summary>
     public partial class MainWindow : Window
     {
+        private EFContext _db;
         public MainWindow()
         {
             InitializeComponent();
+            _db = new EFContext();
+            _db.People.Count();
+        }
+
+        private void BtnEnterAmount_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbAmountUsers.Text.Length == 0)
+            {
+                MessageBox.Show("Enter amount of people", "Error");
+                return;
+            }
+            int amount = 0;
+
+            try
+            {
+                amount = Convert.ToInt32(tbAmountUsers.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Incorrect amount of people", "Error");
+                return;
+            }
+
+            List<Entities.Person> randUsers = null;
+            randUsers = CreateRandPeople(amount);
+
+            try
+            {
+                _db.People.AddRange(randUsers);
+                _db.SaveChanges();
+                MessageBox.Show("Succeed!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something wrong!", "Error");
+            }
         }
     }
 }
