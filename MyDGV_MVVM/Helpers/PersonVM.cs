9d8afb1af8ee9a3de380c83dbea41f372bcb761a
Stepan <stepansmetanskyy@gmail.com>
2019-10-03 19:48:15 +0300

    Added class PersonVM : INotifyPropertyChanged

---
+++ MyDGV_MVVM/Helpers/PersonVM.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDGV_MVVM.Helpers
+{
+    class PersonVM : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        private string _surname;
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                if (_surname != value)
+                {
+                    _surname = value;
+                    this.NotifyPropertyChanged("Surname");
+                }
+            }
+        }
+
+        private DateTime _birthdate;
+        public DateTime Birthdate
+        {
+            get { return _birthdate; }
+            set
+            {
+                if (_birthdate != value)
+                {
+                    _birthdate = value;
+                    this.NotifyPropertyChanged("Birthdate");
+                }
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    this.NotifyPropertyChanged("Email");
+                }
+            }
+        }
+
+        private string _imageUrl;
+        public string ImageUrl
+        {
+            get { return _imageUrl; }
+            set
+            {
+                if (_imageUrl != value)
+                {
+                    _imageUrl = value;
+                    this.NotifyPropertyChanged("ImageUrl");
+                }
+            }
+        }
+
+        private string _gender;
+        public string Gender
+        {
+            get { return _gender; }
+            set
+            {
+                if (_gender != value)
+                {
+                    _gender = value;
+                    this.NotifyPropertyChanged("Gender");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
